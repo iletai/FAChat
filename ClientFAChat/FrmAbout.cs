370dc706d8580a3b911bceb9d5d52c4029d0d932
letaii <letaii@hotmail.com>
2017-12-21 03:58:19 +0700

    fist

---
+++ ClientFAChat/FrmAbout.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace FAChatClient
+{
+    public partial class FrmAbout : Form
+    {
+        public FrmAbout()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/letaii");
+        }
+    }
+}
---
+++ ClientLibraryFAChat/Client.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace FAChatClientLibrary
+{/// <summary>
+/// Class with holds all the information of the logged in client
+/// </summary>
+    public static class Client
+    {
+        public static string UserName;
+        public static string Name;
+        public static Color Color;
+    }
+}
